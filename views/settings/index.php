75d6770f0b4665adaadc4f0e2e1b35727d6fb018
Brennan Novak <brennan@reverseproductions.com>
2011-07-04 18:22:12 -0700

    changed settings page

--- views/settings/index.php
+++ views/settings/index.php
@@ -2,8 +2,9 @@
 <div class="content_wrap_inner">
 
 	<div class="content_inner_top_right">
-		<h3>Module</h3>
+		<h3>App</h3>
 		<p><?= form_dropdown('enabled', config_item('enable_disable'), $settings['blog']['enabled']) ?></p>
+		<p><a href="<?= base_url() ?>api/<?= $this_module ?>/uninstall" id="app_uninstall" class="button_delete">Uninstall</a></p>
 	</div>
 
 	<h3>Permissions</h3>
@@ -81,7 +82,7 @@
 	<?= form_dropdown('comments_per_page', config_item('amount_increments_five'), $settings['blog']['comments_per_page']) ?>
 	</p>
 
-	<input type="hidden" name="module" value="blog">
+	<input type="hidden" name="module" value="<?= $this_module ?>">
 
 	<p><input type="submit" name="save" value="Save" /></p>
 
