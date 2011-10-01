004da2b62b72505e8b4a46359d6f7414ff715530
Brennan Novak <brennan@reverseproductions.com>
2011-10-01 13:18:57 -0700

    commiting latest, js fixes, etc...

--- controllers/settings.php
+++ controllers/settings.php
@@ -14,7 +14,13 @@ class Settings extends Dashboard_Controller
     }
  
  	function index()
-	{ 	
+	{
+		if (config_item('blog_enabled') == '') 
+		{
+			$this->session->set_flashdata('message', 'Oops, the Blog app is not installed');
+			redirect('settings/apps');
+		}
+	
 		$this->data['sub_title'] 	= 'Settings';
 		$this->data['shared_ajax'] .= $this->load->view(config_item('dashboard_theme').'/partials/settings_modules_ajax.php', $this->data, true);		
 		$this->render('dashboard_wide');
