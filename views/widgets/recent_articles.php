75855cd101b7b6083ae85a72493e80d62e0072a3
Brennan Novak <brennan@reverseproductions.com>
2012-02-19 17:08:26 -0800

    fixed widget errors

--- views/widgets/recent_articles.php
+++ views/widgets/recent_articles.php
@@ -1,9 +1,9 @@
 <div class="widget_<?= $widget_region ?> widget_blog_recent_articles" id="widget_<?= $widget_id ?>">
 	<h2><?= $widget_title ?></h2>
 	<ul>
-	<?php if ($posts): foreach ($posts as $post): $post_link = post_link($blog_path, $url_style, $post->created_at, $post->title_url); ?>
+	<?php if ($posts): foreach ($posts as $post): $post_link = post_link(config_item('blog_path'), config_item('blog_url_style'), $post->created_at, $post->title_url); ?>
 		<li><a href="<?= $post_link ?>"><?= $post->title ?></a></li>
-		<li><?= human_date($date_style, mysql_to_unix($post->created_at)) ?></li>
+		<li><?= human_date(config_item('blog_date_style'), mysql_to_unix($post->created_at)) ?></li>
 		<li>&nbsp;</li>
 	<?php endforeach; else: ?>
 		<li>No Articles Have Been Published</li>
