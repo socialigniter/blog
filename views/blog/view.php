2f60f041a3a08a787e41f86bf0fb7d0b82a9a9f1
Brennan Novak <brennan@reverseproductions.com>
2012-06-11 23:03:57 -0700

    made share widgets use https

--- views/blog/view.php
+++ views/blog/view.php
@@ -10,8 +10,8 @@
 		<table border="0" cellpadding="4" cellspacing="0">
 		<tr>
 			<td><script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script><g:plusone size="medium" href="<?= $article_link ?>"></g:plusone></td>
-			<td><a href="http://twitter.com/share" class="twitter-share-button" data-url="<?= $article_link ?>" data-text="<?= $article->title ?>" data-count="horizontal" data-via="<?= config_item('twitter_default_account') ?>">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></td>
-			<td><iframe src="//www.facebook.com/plugins/like.php?href=<?= $article_link ?>&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&amp;appId=112321278779214" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe></td>
+			<td><a href="https://twitter.com/share" class="twitter-share-button" data-url="<?= $article_link ?>" data-text="<?= $article->title ?>" data-count="horizontal" data-via="<?= config_item('twitter_default_account') ?>">Tweet</a><script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script></td>
+			<td><iframe src="https://www.facebook.com/plugins/like.php?href=<?= $article_link ?>&amp;send=false&amp;layout=button_count&amp;width=250&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&amp;appId=112321278779214" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe></td>
 		</tr>
 		</table>	
 	</div>
--- views/partials/post_actions.php
+++ views/partials/post_actions.php
@@ -1,7 +1,7 @@
 <div class="post_actions">
-	<a href="http://twitter.com/home?status=<?php echo urlencode($row_Blog['title'])."+".$PostLink; ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/twitter.png" border="0" /></a>
-	<a href="http://facebook.com/sharer.php?u=<?php echo $PostLink; ?>&t=<?php echo urlencode($row_Blog['title']) ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/facebook.png" border="0" /></a>
-	<a href="http://delicious.com/post?url=<?php echo $PostLink; ?>&title=<?php echo $row_Blog['title']; ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/delicious.png" border="0" /></a>
-	<a href="http://digg.com/submit?phase=2&url=<?php echo $PostLink; ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/digg.png" border="0" /></a>  
-	<a href="http://stumbleupon.com/url/<?php echo $PostLink; ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/stumbleupon.png" border="0" /></a>
+	<a href="https://twitter.com/home?status=<?php echo urlencode($row_Blog['title'])."+".$PostLink; ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/twitter.png" border="0" /></a>
+	<a href="https://facebook.com/sharer.php?u=<?php echo $PostLink; ?>&t=<?php echo urlencode($row_Blog['title']) ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/facebook.png" border="0" /></a>
+	<a href="https://delicious.com/post?url=<?php echo $PostLink; ?>&title=<?php echo $row_Blog['title']; ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/delicious.png" border="0" /></a>
+	<a href="https://digg.com/submit?phase=2&url=<?php echo $PostLink; ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/digg.png" border="0" /></a>  
+	<a href="https://stumbleupon.com/url/<?php echo $PostLink; ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/stumbleupon.png" border="0" /></a>
 </div>
