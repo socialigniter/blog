0f172703701cf67cbe9dd99822e2b86159f0f726
Brennan Novak <hi@brennannovak.com>
2012-12-15 20:31:12 -0800

    added app.json for configuration and installs

--- views/partials/post_actions.php
+++ views/partials/post_actions.php
@@ -1,7 +1,11 @@
-<div class="post_actions">
-	<a href="https://twitter.com/home?status=<?php echo urlencode($row_Blog['title'])."+".$PostLink; ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/twitter.png" border="0" /></a>
-	<a href="https://facebook.com/sharer.php?u=<?php echo $PostLink; ?>&t=<?php echo urlencode($row_Blog['title']) ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/facebook.png" border="0" /></a>
-	<a href="https://delicious.com/post?url=<?php echo $PostLink; ?>&title=<?php echo $row_Blog['title']; ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/delicious.png" border="0" /></a>
-	<a href="https://digg.com/submit?phase=2&url=<?php echo $PostLink; ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/digg.png" border="0" /></a>  
-	<a href="https://stumbleupon.com/url/<?php echo $PostLink; ?>" target="_blank"><img src="<?php echo $CFG->images; ?>social_icons/stumbleupon.png" border="0" /></a>
+<div class="content_actions">
+	<h3>Share</h3>	
+
+	<table border="0" cellpadding="4" width="215">
+	<tr>
+		<td><script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script><g:plusone size="medium" href="<?= $article_link ?>"></g:plusone></td>		
+		<td><a href="https://twitter.com/share" class="twitter-share-button" data-url="<?= $article_link ?>" data-text="<?= $article->title ?>" data-count="horizontal" data-via="<?= config_item('twitter_default_account') ?>">Tweet</a><script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script></td>
+		<td><iframe src="https://www.facebook.com/plugins/like.php?href=<?= $article_link ?>&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21&amp;appId=112321278779214" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe></td>
+	</tr>
+	</table>
 </div>
